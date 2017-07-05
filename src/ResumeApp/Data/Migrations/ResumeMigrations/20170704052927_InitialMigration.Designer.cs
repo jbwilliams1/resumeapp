c8f24520ad7e0e408b4bbb4fa84ee4ba94c1168d
Jacob Williams <jbwilliams1@gmail.com>
2017-07-05 09:23:19 -0500

    Add Initial EF Migration

---
+++ src/ResumeApp/Data/Migrations/ResumeMigrations/20170704052927_InitialMigration.Designer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ResumeApp.Data;
+
+namespace ResumeApp.Data.Migrations.ResumeMigrations
+{
+    [DbContext(typeof(ResumeContext))]
+    [Migration("20170704052927_InitialMigration")]
+    partial class InitialMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn)
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("ResumeApp.Models.Employee", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Address");
+
+                    b.Property<string>("Biography");
+
+                    b.Property<string>("City");
+
+                    b.Property<string>("FirstName");
+
+                    b.Property<string>("LastName");
+
+                    b.Property<string>("Phone");
+
+                    b.Property<string>("State");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Employees");
+                });
+
+            modelBuilder.Entity("ResumeApp.Models.Job", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("CompanyName");
+
+                    b.Property<string>("Description");
+
+                    b.Property<int?>("EmployeeID");
+
+                    b.Property<DateTime?>("EndDate");
+
+                    b.Property<string>("ImageUrl");
+
+                    b.Property<DateTime>("StartDate");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("ID");
+
+                    b.HasIndex("EmployeeID");
+
+                    b.ToTable("Jobs");
+                });
+
+            modelBuilder.Entity("ResumeApp.Models.Skill", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<int?>("EmployeeID");
+
+                    b.Property<string>("Name");
+
+                    b.Property<int>("Proficiency");
+
+                    b.HasKey("ID");
+
+                    b.HasIndex("EmployeeID");
+
+                    b.ToTable("Skills");
+                });
+
+            modelBuilder.Entity("ResumeApp.Models.Job", b =>
+                {
+                    b.HasOne("ResumeApp.Models.Employee", "Employee")
+                        .WithMany("Jobs")
+                        .HasForeignKey("EmployeeID");
+                });
+
+            modelBuilder.Entity("ResumeApp.Models.Skill", b =>
+                {
+                    b.HasOne("ResumeApp.Models.Employee", "Employee")
+                        .WithMany("Skills")
+                        .HasForeignKey("EmployeeID");
+                });
+        }
+    }
+}
---
+++ src/ResumeApp/Data/Migrations/ResumeMigrations/20170704052927_InitialMigration.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ResumeApp.Data.Migrations.ResumeMigrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Address = table.Column<string>(nullable: true),
+                    Biography = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    CompanyName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    EmployeeID = table.Column<int>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true),                
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Jobs_Employees_EmployeeID",
+                        column: x => x.EmployeeID,
+                        principalTable: "Employees",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Description = table.Column<string>(nullable: true),
+                    EmployeeID = table.Column<int>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Proficiency = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Skills_Employees_EmployeeID",
+                        column: x => x.EmployeeID,
+                        principalTable: "Employees",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_EmployeeID",
+                table: "Jobs",
+                column: "EmployeeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skills_EmployeeID",
+                table: "Skills",
+                column: "EmployeeID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jobs");
+
+            migrationBuilder.DropTable(
+                name: "Skills");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
