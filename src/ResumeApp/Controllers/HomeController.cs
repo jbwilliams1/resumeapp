874f4388a3dd8eb2f27c8b03feda00cee2419d91
Jacob Williams <jbwilliams1@gmail.com>
2017-07-05 09:23:47 -0500

    Add Controllers

---
+++ src/ResumeApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ResumeApp.Data;
+
+namespace ResumeApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ResumeContext db;
+        private readonly ILogger _logger;
+
+        public HomeController(ResumeContext context,
+            ILogger<HomeController> logger)
+        {        
+            db = context;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {        
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetEmployeeResume(int EmployeeID)
+        {
+            
+
+        }
+    }
+}
