2e8ef796314f5ac17ebdb521e7f8cd76472a4b06
Jacob Williams <jbwilliams1@gmail.com>
2017-07-05 09:18:27 -0500

    Add NPGSQL (PostgreSQL) connection service

--- src/ResumeApp/Startup.cs
+++ src/ResumeApp/Startup.cs
@@ -31,7 +31,7 @@ namespace ResumeApp
         {
             // Add framework services.
             services.AddMvc();
-            
+            services.AddEntityFrameworkNpgsql().AddDbContext<ResumeContext>(options => options.UseNpgsql(Configuration.GetConnectionString("ResumeDatabase")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
