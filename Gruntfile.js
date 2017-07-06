0104c34313dc729afda08b7fe18121afffa29446
Jacob Williams <jbwilliams1@gmail.com>
2017-07-06 01:36:05 -0500

    Add less and uglify to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,40 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-   
+    less : {
+      production : {
+        options: {
+          compress: true,
+          yuicompress: false,
+          optimization: 2,
+          cleancss:false,  
+          paths: ["src/ResumeApp/wwwroot/css/"],   
+          syncImport: false,
+          strictUnits:false,
+          strictMath: true,
+          strictImports: true,
+          ieCompat: false    
+        },
+        files : {
+          'src/ResumeApp/wwwroot/css/site.css' : 'src/ResumeApp/Assets/Less/site.less'
+        }
+      }
+    },
+    uglify : {
+      options : {
+        mangle : false
+      },
+      my_target : {
+        files : {
+          'src/ResumeApp/wwwroot/js/site.min.js' : ['node_modules/angular/angular.min.js', 'src/ResumeApp/Assets/JS/resume.js']
+        }
+      }
+    }
   });
+
+
+  grunt.registerTask('default', ['less:production', 'uglify']);
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-copy');
 };
